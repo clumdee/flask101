211b8981453fd5210c983933f72a18a57bb331ac
clumdee <chatdanai.l@gmail.com>
2022-05-03 22:07:39 +0700

    sample usages

--- flask101/app.py
+++ flask101/app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
+
 
 app = Flask(__name__)
 
+
+# simple endpoint
 @app.route("/")
 def home():
     return "This is a home page"
+
+# pass part of URL as an argument
+@app.route("/<name>")
+def user(name):
+    return f"Hello {name}!"
+
+# straight redirect / redirect with url_for
+@app.route("/admin")
+def admin():
+    # return redirect("/not-you")
+    return redirect(url_for("user", name="not admin"))
+
 
 if __name__ == '__main__':
     app.run()
