8bb7b201222146963a3ad47b79b8a0b9be1813e4
clumdee <chatdanai.l@gmail.com>
2022-05-03 22:28:57 +0700

    simple log

--- flask101/app.py
+++ flask101/app.py
@@ -1,5 +1,15 @@
+import logging
 from flask import Flask, redirect, url_for
 
+# simple log
+logger_name = "app"
+logger = logging.getLogger(logger_name)
+logger.setLevel(logging.DEBUG)
+# location w.r.t. working directory that calls this Flask app
+hdler = logging.FileHandler(f"{logger_name}.log")
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+hdler.setFormatter(formatter)
+logger.addHandler(hdler)
 
 app = Flask(__name__)
 
@@ -7,6 +17,7 @@ app = Flask(__name__)
 # simple endpoint
 @app.route("/")
 def home():
+    logger.debug("calling home")
     return "This is a home page"
 
 # pass part of URL as an argument
