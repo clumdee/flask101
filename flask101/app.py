c35b7765af97111b14a23d23f3d61d3d62f183f3
clumdee <chatdanai.l@gmail.com>
2022-05-16 12:13:31 +0700

    create data model / table

--- flask101/app.py
+++ flask101/app.py
@@ -34,6 +34,17 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # optional, suppress noti
 # define SQLAlchemy object
 db = SQLAlchemy(app)
 
+# create a model (table) for users
+class users(db.Model):
+    _id = db.Column('id', db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+
 # simple endpoint
 @app.get('/')
 def home():
